d8623ad2dab3c4e93c193e181c4f6ccc5128c5d5
Harumaro <43531665+HarumaroJP@users.noreply.github.com>
2021-07-22 21:00:50 +0900

    add Roomfilter

---
+++ RoomFilter.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using SoftGear.Strix.Client.Core.Model.Manager.Filter;
+using SoftGear.Strix.Client.Core.Model.Manager.Filter.Builder;
+
+namespace StrixEx {
+    public struct RoomFilter {
+        IDictionary<string, object> dictionary;
+        public string roomName;
+        public int maxPlayer;
+        public int mapKey;
+
+        public static RoomFilter Create(string roomName, int capacity, int mapKey) {
+            return new RoomFilter() {
+                roomName = roomName,
+                maxPlayer = capacity,
+                mapKey = mapKey
+            };
+        }
+
+        public readonly ICondition ToCondition() {
+            IConditionBuilder builder;
+
+            builder = ConditionBuilder.Builder().Field("capacity").EqualTo(maxPlayer);
+            builder.And().Field("key1").EqualTo((double) mapKey);
+
+            return builder.Build();
+        }
+
+        public IDictionary<string, object> ToDictionary() {
+            dictionary = new Dictionary<string, object> {
+                {"name", roomName},
+                {"capacity", maxPlayer},
+                {"password", ""},
+                {"state", 0},
+                {"isJoinable", true},
+                {"key1", (double) mapKey},
+            };
+
+            return dictionary;
+        }
+    }
+}
